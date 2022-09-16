961f1a45f82e438990cecb38a0f472b6462eb56d
Jiny-world <hellozin31@naver.com>
2022-09-16 16:18:27 +0900

    슬라이더 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -18,51 +18,79 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(253, 121, 168), rgb(250, 177, 160));
+  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 26px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  position: absolute;
 `;
 
 const boxVariants = {
-  initial: {
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
     opacity: 0,
-    y: 50,
-  },
+    scale: 0,
+    transition: {
+      duration: 1,
+    },
+  }),
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [isBack, setIsBack] = useState(false);
+  const prevBtn = () => {
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    setIsBack(true);
   };
+  const nextBtn = () => {
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+    setIsBack(false);
+  };
+
   return (
     <Wrapper>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+      <AnimatePresence
+        custom={isBack}
+        exitBeforeEnter // exit 가 완전히 실행 된 후 enter 를 실행함
+      >
+        <Box
+          custom={isBack}
+          key={visible}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit"
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click</button>
+      <div>
+        <button onClick={prevBtn}>Prev</button>
+        <button onClick={nextBtn}>Next</button>
+      </div>
     </Wrapper>
   );
 }
