3dfef47d4772155cd15321ca4cc63b82d63db5b0
Jiny-world <hellozin31@naver.com>
2022-09-16 16:46:05 +0900

    컴포넌트의 layoutId 속성으로 애니메이션 연결

--- src/App.tsx
+++ src/App.tsx
@@ -13,84 +13,46 @@ const Wrapper = styled(motion.div)`
   display: flex;
   height: 100vh;
   width: 100vw;
-  margin: 0 auto;
-  display: flex;
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(253, 121, 168), rgb(250, 177, 160));
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.9);
   border-radius: 40px;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 26px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
+  margin: 50px;
 `;
 
-const boxVariants = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 1,
-    },
-  }),
-};
-
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
+  background-color: #0984e3;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-  const prevBtn = () => {
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    setIsBack(true);
+  const [clicked, setClicked] = useState(false);
+  const toggleCilcked = () => {
+    setClicked((prev) => !prev);
   };
-  const nextBtn = () => {
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    setIsBack(false);
-  };
-
   return (
-    <Wrapper>
-      <AnimatePresence
-        custom={isBack}
-        exitBeforeEnter // exit 가 완전히 실행 된 후 enter 를 실행함
-      >
-        <Box
-          custom={isBack}
-          key={visible}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <div>
-        <button onClick={prevBtn}>Prev</button>
-        <button onClick={nextBtn}>Next</button>
-      </div>
+    <Wrapper onClick={toggleCilcked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: "50px" }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: "0px" }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
