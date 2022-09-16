c40a4c7f0f614e2d9167362ef9799db1827d9bcc
Jiny-world <hellozin31@naver.com>
2022-09-16 15:21:59 +0900

    AnimatePresence 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,76 @@
-import { createGlobalStyle } from "styled-components";
-const GlobalStyle = createGlobalStyle`
- @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
+import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useScroll,
+  useTransform,
+  Variants,
+  AnimatePresence,
+} from "framer-motion";
+import { useState } from "react";
 
-body{
-  font-family: 'Ubuntu', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-}
-
-a{
-  text-decoration: none;
-  color:inherit;
-}
-
-*{
-  box-sizing:  border-box;
-}
+const Wrapper = styled(motion.div)`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, rgb(253, 121, 168), rgb(250, 177, 160));
+`;
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    y: 50,
+  },
+};
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => {
+    setShowing((prev) => !prev);
+  };
   return (
-    <>
-      <GlobalStyle />
-    </>
+    <Wrapper>
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="initial"
+            animate="visible"
+            exit="leaving"
+          />
+        ) : null}
+      </AnimatePresence>
+      <button onClick={toggleShowing}>Click</button>
+    </Wrapper>
   );
 }
 
 export default App;
-{
-  /* <ReactQueryDevtools initialIsOpen={true} /> */
-}
-{
-  /*  ReactQuery 캐시 저장된데이터를 시각화해줌 */
-}
+/* <ReactQueryDevtools initialIsOpen={true} /> */
+/*  ReactQuery 캐시 저장된데이터를 시각화해줌 */
+
+// transition={{ type: "spring", bounce: 0.8 }}
+// initial={{ scale: 0 }}  초기스타일
+// animate={{ scale: 1, rotateZ: 360 }}   최종스타일
